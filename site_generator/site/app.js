f6df46e2f69be582d9ff0cada101f9b167a9fb92
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2021-04-15 18:53:24 +0530

    Add graph visualization

--- site_generator/site/app.js
+++ site_generator/site/app.js
@@ -4,5 +4,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'pages')))
 
+app.get('/graph', (_, res) => {
+    res.sendFile(path.join(__dirname, 'graph.html'));
+});
+
+app.get('/data', (_, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile(path.join(__dirname, '..', 'site_data.json'));
+});
+
 app.listen(5000);
 console.log('Server is listening on http://localhost:5000');
