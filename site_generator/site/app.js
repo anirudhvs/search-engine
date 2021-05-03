9583c825c0b7043b2028457744b97235f9aec47b
indreshp135 <indreshp135@gmail.com>
2021-05-03 13:36:06 +0530

    refactor: prettier

--- site_generator/site/app.js
+++ site_generator/site/app.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const trie = require("./trie");
-const radix = require('./radix');
-const map = require('./map');
+const radix = require("./radix");
+const map = require("./map");
 const scraper = require("./scraper");
 const pageRank = require("./pagerank");
 const ranking = pageRank(100, 0.85);
@@ -18,60 +18,60 @@ app.use(bodyParser.json());
 scraper.bfs();
 
 function sortByRank(site1, site2) {
-	if (ranking[site1] < ranking[site2]) {
-		return 1;
-	} else if (ranking[site1] > ranking[site2]) {
-		return -1;
-	} else {
-		return 0;
-	}
+  if (ranking[site1] < ranking[site2]) {
+    return 1;
+  } else if (ranking[site1] > ranking[site2]) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 app.post("/search/trie", (req, res) => {
-	head = trie.trieHead.search(req.body.word);
-	ptag = trie.triePtag.search(req.body.word);
-	let union = [...new Set([...ptag, ...head])];
-	union.sort(sortByRank);
-	res.send(union);
+  head = trie.trieHead.search(req.body.word);
+  ptag = trie.triePtag.search(req.body.word);
+  let union = [...new Set([...ptag, ...head])];
+  union.sort(sortByRank);
+  res.send(union);
 });
 
 app.post("/search/radix", (req, res) => {
-	head = radix.radixTreeHead.search(req.body.word);
-	ptag = radix.radixTreePtag.search(req.body.word);
-	let union = [...new Set([...ptag, ...head])];
-	union.sort(sortByRank);
-	res.send(union);
+  head = radix.radixTreeHead.search(req.body.word);
+  ptag = radix.radixTreePtag.search(req.body.word);
+  let union = [...new Set([...ptag, ...head])];
+  union.sort(sortByRank);
+  res.send(union);
 });
 
 app.post("/search/map", (req, res) => {
-	head = map.mapHead.search(req.body.word);
-	ptag = map.mapPtag.search(req.body.word);
-	let union = [...new Set([...ptag, ...head])];
-	union.sort(sortByRank);
-	res.send(union);
+  head = map.mapHead.search(req.body.word);
+  ptag = map.mapPtag.search(req.body.word);
+  let union = [...new Set([...ptag, ...head])];
+  union.sort(sortByRank);
+  res.send(union);
 });
 
 app.post("/words", (req, res) => {
-	head = trie.trieHead.suggest(req.body.snip);
-	ptag = trie.triePtag.suggest(req.body.snip);
-	let union = [...new Set([...ptag, ...head])];
-	union.sort(sortByRank);
-	res.send(union);
+  head = trie.trieHead.suggest(req.body.snip);
+  ptag = trie.triePtag.suggest(req.body.snip);
+  let union = [...new Set([...ptag, ...head])];
+  union.sort(sortByRank);
+  res.send(union);
 });
 
 app.get("/", (_, res) => {
-	res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.use(express.static(path.join(__dirname, "pages")));
 
 app.get("/graph", (_, res) => {
-	res.sendFile(path.join(__dirname, "graph.html"));
+  res.sendFile(path.join(__dirname, "graph.html"));
 });
 
 app.get("/data", (_, res) => {
-	res.setHeader("Content-Type", "application/json");
-	res.sendFile(path.join(__dirname, "..", "site_data.json"));
+  res.setHeader("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "..", "site_data.json"));
 });
 
 app.listen(5000);
