1b281b1527f3ec7d4fccb0a6fc479f5412152540
indreshp135 <indreshp135@gmail.com>
2021-05-20 10:01:13 +0530

    c4

--- app.js
+++ app.js
@@ -96,5 +96,7 @@ app.get("/visualization", (_, res) => {
     res.sendFile(path.join(__dirname, "visualization.html"));
 });
 
-app.listen(process.env.PORT || 5000);
-console.log("Server is listening on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+console.log(`Server is listening on http://localhost:${PORT}`);
